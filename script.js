a0591c06034adbdf824fe5fe21068d695aafbda4
Korch <stephaniekorch@quickenloans.com>
2021-06-09 23:21:22 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//conditions exercise 1
+
+var actualTemp = 82; // number
+
+var desiredTemp = 70; // number
+
+if (actualTemp > desiredTemp) {
+  // run A/C
+  console.log("Run A/C");
+} else if (actualTemp < desiredTemp) {
+  // run heat
+  console.log("Run Heat");
+} else {
+  // standby
+  console.log("Standby");
+}
